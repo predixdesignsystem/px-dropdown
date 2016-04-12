b3de40842d835a58b6a2736ccf99620353ae4055
FrenchBen <benoit.chevalier@ge.com>
2016-04-12 10:14:22 -0700

    enable travis

--- wct.conf.js
+++ wct.conf.js
@@ -5,7 +5,26 @@ module.exports = {
       browsers: ['chrome', 'firefox']
     },
     sauce: {
-      disabled: true
+      disabled: true,
+      "browsers": [{
+          "browserName": "microsoftedge",
+          "platform": "Windows 10",
+          "version": ""
+        }, {
+          "browserName": "internet explorer",
+          "platform": "Windows 8.1",
+          "version": "11"
+        },
+        {
+          "browserName": "safari",
+          "platform": "OS X 10.11",
+          "version": "9"
+        }, {
+          "browserName": "safari",
+          "platform": "OS X 10.10",
+          "version": "8"
+        }
+      ]
     }
   },
   suites: [
